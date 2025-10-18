6afd3b7ef8ad9e02e2edaa0ddb43c8521677574a
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2025-10-18 20:32:07 +0500

    server action created for add to watchlist btn

--- app/(root)/stocks/[symbol]/page.tsx
+++ app/(root)/stocks/[symbol]/page.tsx
@@ -1,5 +1,9 @@
-import TradingViewWidget from '@/components/TradingViewWidget';
-import WatchlistButton from '@/components/WatchlistButton';
+import { auth } from "@/lib/better-auth/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+import { isInWatchlist } from "@/lib/actions/watchlist.actions";
+import TradingViewWidget from "@/components/TradingViewWidget";
+import WatchlistButton from "@/components/WatchlistButton";
 import {
   SYMBOL_INFO_WIDGET_CONFIG,
   CANDLE_CHART_WIDGET_CONFIG,
@@ -7,38 +11,53 @@ import {
   TECHNICAL_ANALYSIS_WIDGET_CONFIG,
   COMPANY_PROFILE_WIDGET_CONFIG,
   COMPANY_FINANCIALS_WIDGET_CONFIG,
-} from '@/lib/constants';
+} from "@/lib/constants";
 
 export default async function StockDetails({ params }: StockDetailsPageProps) {
   const { symbol } = await params;
   const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`;
 
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (!session?.user) redirect("/sign-in");
+  const email = session?.user?.email;
+
+  const inWatchlist = email ? await isInWatchlist(email, symbol) : false;
+
   return (
-    <div className='flex min-h-screen p-4 md:p-6 lg:p-8'>
-      <section className='grid grid-cols-1 md:grid-cols-2 gap-8 w-full'>
+    <div className="flex min-h-screen p-4 md:p-6 lg:p-8">
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
         {/* Left column */}
-        <div className='flex flex-col gap-6'>
-          <TradingViewWidget scriptUrl={`${scriptUrl}symbol-info.js`} config={SYMBOL_INFO_WIDGET_CONFIG(symbol)} height={170} />
+        <div className="flex flex-col gap-6">
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}symbol-info.js`}
+            config={SYMBOL_INFO_WIDGET_CONFIG(symbol)}
+            height={170}
+          />
 
           <TradingViewWidget
             scriptUrl={`${scriptUrl}advanced-chart.js`}
             config={CANDLE_CHART_WIDGET_CONFIG(symbol)}
-            className='custom-chart'
+            className="custom-chart"
             height={600}
           />
 
           <TradingViewWidget
             scriptUrl={`${scriptUrl}advanced-chart.js`}
             config={BASELINE_WIDGET_CONFIG(symbol)}
-            className='custom-chart'
+            className="custom-chart"
             height={600}
           />
         </div>
 
         {/* Right column */}
-        <div className='flex flex-col gap-6'>
-          <div className='flex items-center justify-between'>
-            <WatchlistButton symbol={symbol.toUpperCase()} company={symbol.toUpperCase()} isInWatchlist={false} />
+        <div className="flex flex-col gap-6">
+          <div className="flex items-center justify-between">
+            <WatchlistButton
+              symbol={symbol.toUpperCase()}
+              company={symbol.toUpperCase()}
+              isInWatchlist={inWatchlist}
+            />
           </div>
 
           <TradingViewWidget
@@ -47,9 +66,17 @@ export default async function StockDetails({ params }: StockDetailsPageProps) {
             height={400}
           />
 
-          <TradingViewWidget scriptUrl={`${scriptUrl}company-profile.js`} config={COMPANY_PROFILE_WIDGET_CONFIG(symbol)} height={440} />
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}company-profile.js`}
+            config={COMPANY_PROFILE_WIDGET_CONFIG(symbol)}
+            height={440}
+          />
 
-          <TradingViewWidget scriptUrl={`${scriptUrl}financials.js`} config={COMPANY_FINANCIALS_WIDGET_CONFIG(symbol)} height={464} />
+          <TradingViewWidget
+            scriptUrl={`${scriptUrl}financials.js`}
+            config={COMPANY_FINANCIALS_WIDGET_CONFIG(symbol)}
+            height={464}
+          />
         </div>
       </section>
     </div>
